5265b4647d9a80ae5eef2943e6846ecc137ddc44
Carlos Alcala <Kobo@Kobo-Mac.local>
2019-11-28 19:44:27 -0600

    :sparkles: added edit feature for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,12 +2,8 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
-
-    def index
-        @articles = Article.all
-    end
-
-    def show
+    
+    def edit
         @article = Article.find(params[:id])
     end
 
@@ -20,6 +16,24 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
+
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
+      end
 
     private
     def article_params
